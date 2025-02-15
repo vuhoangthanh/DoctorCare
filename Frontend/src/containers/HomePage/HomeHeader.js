8865dae6edf1944f9cccc8151bc8f5478ad792fc
thanh <vuhoangthanh0710@gmail.com>
2025-02-15 16:43:42 +0700

    banner

--- Frontend/src/containers/HomePage/HomeHeader.js
+++ Frontend/src/containers/HomePage/HomeHeader.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomeHeader.scss';
+import logo from '../../assets/images/logo.svg'
 
 class HomeHeader extends Component {
 
@@ -11,7 +12,7 @@ class HomeHeader extends Component {
                     <div className="home-header-content">
                         <div className="left-content">
                             <i className="fas fa-bars"></i>
-                            <div className="header-logo"></div>
+                            <img className="header-logo" src={logo} />
                         </div>
                         <div className="center-content">
                             <div className="child-content">
@@ -41,18 +42,48 @@ class HomeHeader extends Component {
                         </div>
                         <div className="right-content">
                             <div className="support"><i className="fas fa-question-circle"></i> Hỗ trợ</div>
-                            <div className="flag">VN</div>
+                            <div className="language-vn">VN</div>
+                            <div className="language-en">EN</div>
                         </div>
                     </div>
                 </div>
                 <div className="home-header-banner">
-                    <div className="title1">NỀN TẢNG Y TẾ</div>
-                    <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
-                    <div className="search">
-                        <i className="fas fa-search"></i>
-                        <input type="text" />
+                    <div className="content-up">
+                        <div className="title1">NỀN TẢNG Y TẾ</div>
+                        <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                        <div className="search">
+                            <i className="fas fa-search"></i>
+                            <input type="text" placeholder="Tìm kiếm ..." />
+                        </div>
                     </div>
-                    <div className="options"></div>
+                    <div className="content-down">
+                        <div className="options">
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-hospital"></i></div>
+                                <div className="text-child">Khám Chuyên Khoa</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-mobile-alt"></i></div>
+                                <div className="text-child">Khám Từ Xa</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-procedures"></i></div>
+                                <div className="text-child">Khám Tổng Quát</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-flask"></i></div>
+                                <div className="text-child">Khám Xét Nghiệm Y HọcHọc</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-user-md"></i></div>
+                                <div className="text-child">Khám Sức Khỏe Tinh Thần</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-briefcase-medical"></i></div>
+                                <div className="text-child">Khám Nha Khoa</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         );
