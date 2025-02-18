13e6d4ba068bab43095d4b854158332c248925b2
thanh <vuhoangthanh0710@gmail.com>
2025-02-18 20:40:09 +0700

    refactor 1

--- Backend/src/main/java/vn/project/DoctorCare/config/UserDetailsCustom.java
+++ Backend/src/main/java/vn/project/DoctorCare/config/UserDetailsCustom.java
@@ -1,4 +1,4 @@
-package vn.project.DoctorCare.service;
+package vn.project.DoctorCare.config;
 
 import java.util.Collections;
 
@@ -8,6 +8,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import vn.project.DoctorCare.service.UserService;
 
 @Component("userDetailsService")
 public class UserDetailsCustom implements UserDetailsService {
